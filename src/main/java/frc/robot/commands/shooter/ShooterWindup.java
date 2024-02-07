87d367d469f3f88aaf397728bb8a967bf21cec56
Kushagra S <93955597+Ghost-python07@users.noreply.github.com>
2024-02-06 22:43:55 -0500

    Shooter commands full impl. + ShooterWindup minor fixes + impl. of quad detection WIP

--- src/main/java/frc/robot/commands/shooter/ShooterWindup.java
+++ src/main/java/frc/robot/commands/shooter/ShooterWindup.java
@@ -1,21 +1,24 @@
 package frc.robot.commands.shooter;
+
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.shooter.ShooterIO;
-import frc.robot.subsystems.shooter.ShooterIONeo;
+import frc.robot.subsystems.shooter.Shooter;
 
 public class ShooterWindup extends Command {
-    //Variables
+    
     private double velocitySetPoint;
-    private ShooterIONeo Shooter;
-    ShooterWindup(ShooterIONeo subsystem, double setPoint){
+    private final Shooter shooterSubsystem;
+
+    ShooterWindup(Shooter shooterSubsystem, double setPoint){
         velocitySetPoint = setPoint;
-        Shooter = subsystem;
-        addRequirements(Shooter);
+        this.shooterSubsystem = shootersubsystem;
+        addRequirements(shooterSubsystem);
     }
+
     @Override
     public void execute(){
         Shooter.setVelocityRadSec(velocitySetPoint, 0);
     }
+
     @Override
     public boolean isFinished(){
         return true;
