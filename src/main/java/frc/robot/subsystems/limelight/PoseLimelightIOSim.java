50d13d0db4860de74025eb3e7a1d884260c67727
GNSRobotics2638 <GNSRobotics2638@outlook.com>
2024-02-07 19:40:46 -0500

    nothing much (ignore)

--- src/main/java/frc/robot/subsystems/limelight/PoseLimelightIOSim.java
+++ src/main/java/frc/robot/subsystems/limelight/PoseLimelightIOSim.java
@@ -1,9 +1,14 @@
 package frc.robot.subsystems.limelight;
 
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+
 public class PoseLimelightIOSim implements PoseLimelightIO{
     public void updateInputs(PoseLimelightIOInputs inputs) {
         inputs.botpose_wpiblue = new double[]{0, 0, 0, 0, 0, 0, 0};
         inputs.botpose_wpired = new double[]{0, 0, 0, 0, 0, 0, 0};
+        // inputs.botpose_wpired = new Pose3d(0,0,0,new Rotation3d(0,0,0));
+        // inputs.botpose_wpiblue = new Pose3d(0,0,0,new Rotation3d(0,0,0));
         inputs.cl = 0;
         inputs.tv = 0; 
     }
