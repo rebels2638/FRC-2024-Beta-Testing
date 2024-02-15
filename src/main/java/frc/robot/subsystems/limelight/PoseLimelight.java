9b82a631dec50be2ec05c01d03a3a223f5c48f98
Arrowknee <dch3902@gmail.com>
2024-02-15 11:17:56 -0500

    Small change, compile sync
    Signed-off-by: Arrowknee <dch3902@gmail.com>

--- src/main/java/frc/robot/subsystems/limelight/PoseLimelight.java
+++ src/main/java/frc/robot/subsystems/limelight/PoseLimelight.java
@@ -1,6 +1,9 @@
 package frc.robot.subsystems.limelight;
 
+import java.util.Optional;
+
 import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.inputs.LoggableInputs;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
@@ -9,9 +12,9 @@ import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.limelight.PoseLimelightIO.PoseLimelightIOInputs;
-import java.util.Optional;
 
 public class PoseLimelight extends SubsystemBase{
     private final PoseLimelightIOInputs inputs = new PoseLimelightIOInputs();
@@ -20,7 +23,7 @@ public class PoseLimelight extends SubsystemBase{
     public final Optional<DriverStation.Alliance> alliance = DriverStation.getAlliance();
 
     // apriltag constants,, fix all 
-    private final Pose3d defaultShotPoint;
+    private Pose3d defaultShotPoint;
     private final double max_delta_tx = 0; // in degrees
     private final double max_ta = 0; // in meters (i think)
     private final double maxTranslationY = 0.05;
@@ -28,14 +31,13 @@ public class PoseLimelight extends SubsystemBase{
 
     public PoseLimelight(PoseLimelightIO poseLimelightIOReal) {
         this.io = poseLimelightIOReal;
-        defaultShotPoint = (alliance.get() == DriverStation.Alliance.Red) ? new Pose3d(16.579342, 5.547868, 2.1, new Rotation3d(0, 0, 0)) : new Pose3d(-0.0381, 5.547868, 2.1, new Rotation3d(0, 0, 0));
-
+        defaultShotPoint = new Pose3d(0,0,0, new Rotation3d(0,0,0)); //Default
     }
 
     @Override
     public void periodic() {
         io.updateInputs(inputs);
-        // Logger.processInputs("PoseLimelight", inputs);
+        // Logger.processInputs("PoseLimelight", (LoggableInputs)inputs);
     }
 
     public boolean hasValidTargets() {
@@ -56,16 +58,17 @@ public class PoseLimelight extends SubsystemBase{
 
     // apriltag methods
     public Pose3d getValidShotPoint() {
-        
+        defaultShotPoint = (alliance.get() == DriverStation.Alliance.Red) ? new Pose3d(16.579342, 5.547868, 2.1, new Rotation3d(0, 0, 0)) : new Pose3d(-0.0381, 5.547868, 2.1, new Rotation3d(0, 0, 0));
+
         double x = defaultShotPoint.getX();
 
-        // double proportionX = Math.abs(io.getX() / max_delta_tx);
-        // double y = defaultShotPoint.getY() + (proportionX * maxTranslationY);
+        double proportionX = Math.abs(io.getX() / max_delta_tx);
+        double y = defaultShotPoint.getY() + (proportionX * maxTranslationY);
 
         double proportionA = io.getA() / max_ta;
         double z = defaultShotPoint.getZ() + (proportionA * maxTranslationZ);
 
-        return new Pose3d(x, defaultShotPoint.getY(), z, new Rotation3d(0, 0, 0));
+        return new Pose3d(x, y, z, new Rotation3d(0, 0, 0));
     }
 
 }
