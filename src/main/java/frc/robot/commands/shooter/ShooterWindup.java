e668f4bff26143324f2ceb0c10db7174b5c84285
GNSRobotics2638 <gns2638robotics@gmail.com>
2024-02-22 15:02:11 -0500

    AutoPaths working

--- src/main/java/frc/robot/commands/shooter/ShooterWindup.java
+++ src/main/java/frc/robot/commands/shooter/ShooterWindup.java
@@ -26,7 +26,7 @@ public class ShooterWindup extends Command {
 
     @Override
     public boolean isFinished(){
-        return false;
+        return shooterSubsystem.reachedSetpoint();
     }
     
 }
