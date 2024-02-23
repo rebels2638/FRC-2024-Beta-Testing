9534de4404831f6194380f3a4fc6125df8bb15a4
GNSRobotics2638 <gns2638robotics@gmail.com>
2024-02-23 12:18:39 -0500

    l

---
+++ src/main/java/frc/robot/commands/tuning/ForwardAtConstantVelocity.java
@@ -0,0 +1,10 @@
+// package frc.robot.commands.tuning;
+
+// import frc.robot.subsystems.swerve.SwerveSubsystem;
+// import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+// import edu.wpi.first.wpilibj2.command.Command;
+
+// public class ForwardAtConstantVelocity extends Command {
+
+//     private final SysIdRoutine Driveforward
+// }
---
+++ src/main/java/frc/robot/commands/tuning/RotationAtVariableVelocity.java
@@ -0,0 +1,5 @@
+package frc.robot.commands.tuning;
+
+public class RotationAtVariableVelocity {
+    
+}
