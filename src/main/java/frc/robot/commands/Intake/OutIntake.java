c0c42fc04fa7b673688c858b3827b73c5953e8be
erica! <115647009+ejiang2@users.noreply.github.com>
2024-02-23 12:27:39 -0500

    Update OutIntake.java

--- src/main/java/frc/robot/commands/Intake/OutIntake.java
+++ src/main/java/frc/robot/commands/Intake/OutIntake.java
@@ -3,20 +3,21 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.intake.Intake;
 
 public class OutIntake extends Command{
-    private Intake intakeNeo;
     private boolean isIn = false;
 
-    public OutIntake(Intake intakeSubsystem){
-        intakeNeo = intakeSubsystem;
+    public OutIntake(){
     }
+    
     @Override
     public void execute(){
-        isIn = intakeNeo.inIntake();
+        isIn = Intake.getInstance().inIntake();
     }
+    
     @Override
     public void end(boolean isInterrupted){
         return;
     }
+    
     @Override
     public boolean isFinished(){
         return !isIn;
