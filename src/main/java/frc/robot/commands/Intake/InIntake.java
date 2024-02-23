6f34b2454142c682c89976be6530d4a4e0d9fb3e
erica! <115647009+ejiang2@users.noreply.github.com>
2024-02-23 12:33:11 -0500

    Update InIntake.java

--- src/main/java/frc/robot/commands/Intake/InIntake.java
+++ src/main/java/frc/robot/commands/Intake/InIntake.java
@@ -3,14 +3,15 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.intake.Intake;
 
 public class InIntake extends Command{
+    private Intake intakeNeo = Intake.getInstance();
     private boolean isIn = false;
 
-    public InIntake(){
+    public InIntake() {
     }
     
     @Override
     public void execute(){
-        isIn = Intake.getInstance().inIntake();
+        isIn = intakeNeo.inIntake();
     }
     
     @Override
