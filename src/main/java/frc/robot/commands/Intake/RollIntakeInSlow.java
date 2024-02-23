6d33ba95d2faee3e48ddb6dd0461dcb185189a56
erica! <115647009+ejiang2@users.noreply.github.com>
2024-02-23 12:39:14 -0500

    Update RollIntakeInSlow.java

--- src/main/java/frc/robot/commands/Intake/RollIntakeInSlow.java
+++ src/main/java/frc/robot/commands/Intake/RollIntakeInSlow.java
@@ -6,12 +6,9 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.intake.Intake;
 
 public class RollIntakeInSlow extends Command {
-    private final Intake intakeSubsystem;
+    private final Intake intakeSubsystem = Intake.getInstance();
 
-    public RollIntakeInSlow(Intake intakeSubsystem) {
-        this.intakeSubsystem = intakeSubsystem;
-
-        addRequirements(intakeSubsystem);
+    public RollIntakeInSlow() {
     }
 
     @Override
